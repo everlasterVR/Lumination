8fdb9aaa0320efd645aa82d98da5b9a175029e44
everlaster <everlaster@protonmail.com>
2021-02-27 15:02:34 +0200

    AimConstrain: refactoring

--- src/AimConstrain.cs
+++ src/AimConstrain.cs
@@ -26,35 +26,8 @@ namespace Illumination
             SetConstraintActive(true);
         }
 
-        private void OnDisable()
-        {
-            SetConstraintActive(false);
-        }
-
-        private void SetConstraintActive(bool value)
-        {
-            try
-            {
-                if(aimingAtom != null)
-                {
-                    FreeControllerV3 fc = aimingAtom.gameObject.GetComponentInChildren<FreeControllerV3>();
-                    if(fc != null)
-                    {
-                        if(fc.gameObject.GetComponent<AimConstraint>() != null)
-                        {
-                            fc.gameObject.GetComponent<AimConstraint>().constraintActive = value;
-                            Log.Message($"Successfully set constraintActive={value} for {aimingAtom.name}", nameof(AimConstrain));
-                        }
-                    }
-                }
-            }
-            catch(Exception e)
-            {
-                Log.Error($"{e}", nameof(AimConstrain));
-            }
-        }
-
-        private void AddAimConstraintTargetingTransform(Transform targetXForm) //adds an aimconstraint to the containing atom, targeting the targetXForm
+        //adds an aimconstraint to the containing atom, targeting the targetXForm
+        private void AddAimConstraintTargetingTransform(Transform targetXForm)
         {
             //set up constraint source using target transform
             cs.sourceTransform = targetXForm;
@@ -67,39 +40,26 @@ namespace Illumination
                 Log.Message("AimConstraint script needs to be on an atom with an active FreeControllerV3. Make sure atom is not parented.", nameof(AimConstrain));
                 return;
             }
-            if(fc.gameObject.GetComponent<AimConstraint>() == null)
+
+            AimConstraint ac = fc.gameObject.GetComponent<AimConstraint>();
+            if(ac == null)
             {
-                //there is no aimconstraint component yet, so create a new aimconstraint and add a new source
-                AimConstraint ac = fc.gameObject.AddComponent(typeof(AimConstraint)) as AimConstraint;
-            }
-            if(fc.gameObject.GetComponent<AimConstraint>().sourceCount > 1)
-            {
-                //aimconstraint exists, but too many constraint sources, remove them all
-                for(int i = 0; i < fc.gameObject.GetComponent<AimConstraint>().sourceCount; i++)
-                {
-                    fc.gameObject.GetComponent<AimConstraint>().RemoveSource(i);
-                }
+                //there is no aimconstraint component yet, so create a new aimconstraint
+                ac = fc.gameObject.AddComponent(typeof(AimConstraint)) as AimConstraint;
             }
 
-            if(fc.gameObject.GetComponent<AimConstraint>().sourceCount == 1)
-            {
-                //use the existing constraint source
-                fc.gameObject.GetComponent<AimConstraint>().SetSource(0, cs);
-            }
-            else
-            {
-                //no constraint sources, so add a new one
-                fc.gameObject.GetComponent<AimConstraint>().AddSource(cs);
-            }
+            //reset source
+            RemoveSources(ac);
+            ac.AddSource(cs);
+
             //set other aimconstraint parameters
-            fc.gameObject.GetComponent<AimConstraint>().aimVector.Set(0, 0, 1);
-            fc.gameObject.GetComponent<AimConstraint>().upVector.Set(0, 1, 0);
-            fc.gameObject.GetComponent<AimConstraint>().worldUpType = AimConstraint.WorldUpType.None;
-            fc.gameObject.GetComponent<AimConstraint>().weight = 1;
-            fc.gameObject.GetComponent<AimConstraint>().rotationAxis = Axis.X | Axis.Y; //change this to constrain axis
-            fc.gameObject.GetComponent<AimConstraint>().constraintActive = true;
+            ac.aimVector.Set(0, 0, 1);
+            ac.upVector.Set(0, 1, 0);
+            ac.worldUpType = AimConstraint.WorldUpType.None;
+            ac.weight = 1;
+            ac.rotationAxis = Axis.X | Axis.Y; //change this to constrain axis
+            ac.constraintActive = true;
             return;
-
         }
 
         public void DisableAimConstraint() //removes all the constraint sources
@@ -111,19 +71,44 @@ namespace Illumination
                 Log.Message("AimConstraint script needs to be on an atom with an active FreeControllerV3. Make sure atom is not parented.", nameof(AimConstrain));
                 return;
             }
-            if(fc.gameObject.GetComponent<AimConstraint>() == null)
+            AimConstraint ac = fc.gameObject.GetComponent<AimConstraint>();
+            if(ac != null)
             {
-                //there is no aimconstraint so do nothing
-                return;
+                RemoveSources(ac);
+                ac.constraintActive = false;
             }
-            if(fc.gameObject.GetComponent<AimConstraint>().sourceCount > 0)
+        }
+
+        private void OnDisable()
+        {
+            SetConstraintActive(false);
+        }
+
+        private void SetConstraintActive(bool value)
+        {
+            try
             {
-                //remove all constraint sources
-                for(int i = 0; i < fc.gameObject.GetComponent<AimConstraint>().sourceCount; i++)
+                FreeControllerV3 fc = aimingAtom?.gameObject.GetComponentInChildren<FreeControllerV3>();
+                AimConstraint ac = fc?.gameObject.GetComponent<AimConstraint>();
+
+                if(ac != null)
                 {
-                    fc.gameObject.GetComponent<AimConstraint>().RemoveSource(i);
-                    fc.gameObject.GetComponent<AimConstraint>().constraintActive = false;
+                    ac.constraintActive = value;
+                    Log.Message($"Successfully set constraintActive={value} for {aimingAtom.name}", nameof(AimConstrain));
                 }
+            }
+            catch(Exception e)
+            {
+                Log.Error($"{e}", nameof(AimConstrain));
+            }
+        }
+
+        //remove all constraint sources
+        private void RemoveSources(AimConstraint ac)
+        {
+            for(int i = 0; i < ac.sourceCount; i++)
+            {
+                ac.RemoveSource(i);
             }
         }
     }
